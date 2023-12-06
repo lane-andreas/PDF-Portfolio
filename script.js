a9636a623a2a6633cc9b3eb929cd237d20ecde0c
lane-andreas <laaaandreas@gmail.com>
2023-12-06 04:59:31 -0600

    l

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let mainElement = document.querySelector("main");
 let sectionElement = document.querySelector("section");
 let zoomText = document.querySelector("#zoomText");
 let pageCount = document.querySelector("#pageCount");
+let opacityLayer = document.querySelector("#dark");
+let bulb = document.querySelector("#bulb");
 
 document.querySelector("#unzoom").addEventListener("click", function () {
   zoomValue = zoomValue - 0.1; // Increment degrees by 90, and ensure it stays within 0-359
@@ -35,6 +37,18 @@ document.querySelector("#expand").addEventListener("click", function () {
   if (expandValue2 == "50vw") sectionElement.style.width = "95vw";
   else sectionElement.style.width = "50vw";
 });
+
+document.querySelector("#lightbulb").addEventListener("click", function () {
+  var opacityValue = opacityLayer.style.opacity;
+  if (opacityValue == "0") {
+    opacityLayer.style.opacity = "0.4";
+    bulb.setAttribute("icon", "material-symbols:lightbulb-outline");
+  } else {
+    opacityLayer.style.opacity = "0";
+    bulb.setAttribute("icon", "material-symbols:lightbulb");
+  }
+});
+
 mainElement.onmouseover = function () {
   pageCount.textContent = "1 / 2";
 };
